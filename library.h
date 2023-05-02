7358c682a5e20b78b595b7a74a7a80ba03d55b2d
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2023-05-01 19:56:36 -0700

    Create library.h

---
+++ library.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void display(const int arr[], int size);
+
+void add_to_array(int arr[], const int size);
+
+void find_max(const int arr[], const int);
+
+void find_three_maxes(const int arr[], const int);
+
+void insert(int arr[], int &size, const int val);
+
+void selection_sort(int arr[], const int size);
+
+void insertion_sort(int arr[], const int size);
+
+void bubble_sort(int arr[], const int size);
